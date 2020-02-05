ecd09bc38c0400bc5f420e504e52fe527289ff06
Pinker Paweł <ppinker@olivia.biz>
2020-02-05 11:15:18 +0100

    get last element kata

---
+++ main/7kyu/java/SolutionLast.java
@@ -0,0 +1,26 @@
+import java.util.List;
+
+/**
+ * Find the last element of the given argument(s).
+ * Examples
+ *
+ * last(Arrays.asList(1, 2, 3, 4)); // =>  4
+ * last("xyz");                     // => "z"
+ * last(1, 2, 3, 4);                // =>  4
+ * last(new int[]{1, 2, 3, 4});     // =>  4
+ */
+
+public class SolutionLast {
+
+    public static <T> T last(final List<T> list) {
+        return list.get(list.size() -1);
+    }
+
+    public static char last(final String string) {
+        return string.charAt(string.length() - 1);
+    }
+
+    public static <T> T last(final T... list) {
+        return list[list.length - 1];
+    }
+}
